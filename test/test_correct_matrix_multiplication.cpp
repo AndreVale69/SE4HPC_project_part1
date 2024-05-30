6975a65be313c60ebbb3dcb4fcdc8711ed77ef56
AndreVale69 <sforzini31@gmail.com>
2024-05-30 21:02:12 +0200

    Added tests; Added tests

--- test/test_correct_matrix_multiplication.cpp
+++ test/test_correct_matrix_multiplication.cpp
@@ -1,12 +1,24 @@
-#include "matrix_multiplication.h"
 #include <iostream>
 #include <vector>
 #include <gtest/gtest.h>
 
 // ######################### Source code of multiplyMatrices in src/matrix_mult
+void multiplyMatricesWithoutErrors(const std::vector<std::vector<int>> &A,
+                                   const std::vector<std::vector<int>> &B,
+                                   std::vector<std::vector<int>> &C, int rowsA, int colsA,
+                                   int colsB) {
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsB; ++j) {
+            C[i][j] = 0;
+            for (int k = 0; k < colsA; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
 
 
-TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
+TEST(CorrectMatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 3},
         {4, 5, 6}
@@ -18,7 +30,7 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     };
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
 
-    multiplyMatrices(A, B, C, 2, 3, 2);
+    multiplyMatricesWithoutErrors(A, B, C, 2, 3, 2);
 
     std::vector<std::vector<int>> expected = {
         {58, 64},
@@ -29,7 +41,7 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 }
 
 
-TEST(MatrixMultiplicationTest, TestZeroMultiplyMatrices) {
+TEST(CorrectMatrixMultiplicationTest, TestZeroMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
             {0, 0, 0},
             {0, 0, 0}
@@ -41,7 +53,7 @@ TEST(MatrixMultiplicationTest, TestZeroMultiplyMatrices) {
     };
     std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
 
-    multiplyMatrices(A, B, C, 2, 3, 2);
+    multiplyMatricesWithoutErrors(A, B, C, 2, 3, 2);
 
     std::vector<std::vector<int>> expected = {
             {0, 0},
@@ -52,7 +64,7 @@ TEST(MatrixMultiplicationTest, TestZeroMultiplyMatrices) {
 }
 
 
-TEST(MatrixMultiplicationTest, TestIdentityMultiplyMatrices) {
+TEST(CorrectMatrixMultiplicationTest, TestIdentityMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
             {1}
     };
@@ -70,8 +82,3 @@ TEST(MatrixMultiplicationTest, TestIdentityMultiplyMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed!";
 }
 
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
